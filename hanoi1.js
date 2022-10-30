12ded81d834a46db1b4f1b8aedb8388d74f16f39
Arek Jaw <73388566+105730-prog@users.noreply.github.com>
2022-10-30 12:36:35 +0100

    Add files via upload
    Version 1

---
+++ hanoi1.js
@@ -0,0 +1,33 @@
+//global variables
+var margin_rod = 20;
+var el_width = 200;
+var el_midwidth = 60;
+var n = 3;
+var rods = [] ;
+var clicked = " " ;
+var lrod ;
+//starting new game ver1
+function prepareGame(new_n) {
+    var board = 
+document.getElementById("_game");
+    board.innerHTML = " " ;
+    n = new_n
+    rods = [] ;
+for(var i = 1; i>= 1; i--){
+    rods.push([]);
+
+
+}
+function getRod(n) {
+
+}
+function getElement(id,num,col) {
+
+}
+function setElementPos( obj, num, rod){
+
+}
+function ReportState(msg){
+
+}
+// GAME
